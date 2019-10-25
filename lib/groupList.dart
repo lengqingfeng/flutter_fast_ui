b3879a9763a446eca922b7caacc76fac10a5b3ed
srleng <srleng@gaojihealth.com>
2019-10-25 18:09:05 +0800

    add cell tap

--- lib/groupList.dart
+++ lib/groupList.dart
@@ -21,11 +21,9 @@ class _GroupListState extends State<GroupList> {
 
   //构建ListView方法
   Widget _buildListView(context) {
-    var i = 0;
     return new ListView.builder(
       itemBuilder: (context, index) {
         var isGroup = false;
-        i = index;
         if (index == 1 || index == 5 || index == 6) {
           isGroup = true;
         }
@@ -50,51 +48,55 @@ class _GroupListState extends State<GroupList> {
             bottom: BorderSide(
                 width: 10, color: Theme.of(context).scaffoldBackgroundColor)),
       ),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(left: 16),
-            child: new Container(
-              width: 50.0,
-              height: 50.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(6.0),
-                image: DecorationImage(
-                  image: AssetImage("images/item1.jpeg"),
+      child: GestureDetector(
+        onTap: () {
+          print("head tap");
+        },
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: new Container(
+                width: 50.0,
+                height: 50.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(6.0),
+                  image: DecorationImage(
+                    image: AssetImage("images/item1.jpeg"),
+                  ),
                 ),
               ),
             ),
-          ),
-          Expanded(
-              child: Container(
-                  height: 50.0,
-                  margin: EdgeInsets.only(left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Allen',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 17),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            '微信号:lengshengren',
-                            style:
-                                TextStyle(fontSize: 16, color: Colors.black38),
-                          ),
-                          Container(
-                            alignment: Alignment.centerRight,
-                            child: Row(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.insert_chart,
-                                  color: Colors.black45,
-                                ),
+            Expanded(
+                child: Container(
+                    height: 50.0,
+                    margin: EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Allen',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 17),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              '微信号:lengshengren',
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.black38),
+                            ),
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.insert_chart,
+                                    color: Colors.black45,
+                                  ),
 //                                Padding(
 //                                  padding: EdgeInsets.only(right: 5),
 //                                  child: Icon(
@@ -102,16 +104,16 @@ class _GroupListState extends State<GroupList> {
 //                                    color: Colors.black45,
 //                                  ),
 //                               ),
-                                SizedBox(
-                                  width: 5,
-                                ),
-                                Icon(
-                                  Icons.keyboard_arrow_right,
-                                  color: Colors.black45,
-                                ),
-                                SizedBox(
-                                  width: 16,
-                                ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Icon(
+                                    Icons.keyboard_arrow_right,
+                                    color: Colors.black45,
+                                  ),
+                                  SizedBox(
+                                    width: 16,
+                                  ),
 //                               Padding(
 //                                  padding: EdgeInsets.only(right: 16),
 //                                  child: Icon(
@@ -119,72 +121,77 @@ class _GroupListState extends State<GroupList> {
 //                                    color: Colors.black45,
 //                                  ),
 //                                ),
-                              ],
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ))),
-        ],
+                                ],
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ))),
+          ],
+        ),
       ),
     );
   }
 
   //构建Row方法
   Widget _buildRow(value, index, isGroup) {
-    print(value);
-    return new Container(
-      height: 56,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border(
-            bottom: BorderSide(
-                width: isGroup == true ? 10 : 0,
-                color: isGroup == true
-                    ? Theme.of(context).scaffoldBackgroundColor
-                    : Color(0xffffff)),
-          )),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(left: 16.0),
-            child: Image.asset(
-              "images/item1.jpeg",
-              width: 30,
-              height: 30,
-              alignment: Alignment.centerLeft,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(left: 16),
-              decoration: BoxDecoration(
-                  border: Border(
-                bottom: BorderSide(
-                  width: isGroup == true ? 0 : 1,
+    return GestureDetector(
+      onTap: () {
+        print(index);
+      },
+      child: new Container(
+        height: 56,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(
+                  width: isGroup == true ? 10 : 0,
                   color: isGroup == true
-                      ? Color(0xffffff)
-                      : Theme.of(context).scaffoldBackgroundColor,
-                ),
-              )),
-              alignment: Alignment.centerRight,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(value),
-                  Padding(
-                    padding: EdgeInsets.only(right: 16),
-                    child: Icon(
-                      Icons.keyboard_arrow_right,
-                      color: Colors.black45,
-                    ),
-                  )
-                ],
+                      ? Theme.of(context).scaffoldBackgroundColor
+                      : Color(0xffffff)),
+            )),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: Image.asset(
+                "images/item1.jpeg",
+                width: 30,
+                height: 30,
+                alignment: Alignment.centerLeft,
               ),
             ),
-          ),
-        ],
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(left: 16),
+                decoration: BoxDecoration(
+                    border: Border(
+                  bottom: BorderSide(
+                    width: isGroup == true ? 0 : 1,
+                    color: isGroup == true
+                        ? Color(0xffffff)
+                        : Theme.of(context).scaffoldBackgroundColor,
+                  ),
+                )),
+                alignment: Alignment.centerRight,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(value),
+                    Padding(
+                      padding: EdgeInsets.only(right: 16),
+                      child: Icon(
+                        Icons.keyboard_arrow_right,
+                        color: Colors.black45,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
